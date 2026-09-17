1a328a1fa5f9ca92cad6527911dfbcf37a7ecda9
reidssommers-design <reidssommers@gmail.com>
2026-09-17 17:03:50 -0600

    Number Guesser with try counter

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
+        
+        // Console.WriteLine("What is the magic number?");
+        // int magicNum = int.Parse(Console.ReadLine());
+
+        int guessNum;
+        int tries = 0;
+        do
+        {
+            tries ++;
+            Console.WriteLine("What is your guess?");
+            guessNum = int.Parse(Console.ReadLine());
+
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guessNum != magicNum);
+
+        Console.WriteLine($"You guessed it! It only took you {tries} guesses!");
     }
 }
