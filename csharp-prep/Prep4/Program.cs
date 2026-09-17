ec14d1973a6377fb1d5bc076c21f1c839a784ce5
reidssommers-design <reidssommers@gmail.com>
2026-09-17 17:48:55 -0600

    Number list parser

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<string> numbers = new List<string>();
+
+        string number;
+
+        do
+        {
+            Console.WriteLine("Enter a number");
+            number = Console.ReadLine();
+
+            if (number != "0")
+            {
+                numbers.Add(number);
+            }
+        } while (number != "0");
+
+        int total = numbers
+            .Select(int.Parse)
+            .Sum();
+
+        Console.WriteLine($"Total: {total}");
+
+        double average;
+        average = (double)total / numbers.Count;
+        Console.WriteLine($"Average: {average}");
+
+        int max = numbers
+            .Select(int.Parse)
+            .Max();
+
+        Console.WriteLine($"Max: {max}");
+
     }
 }
