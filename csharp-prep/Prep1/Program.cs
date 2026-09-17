d2b6b8fd9653670dd7c0be3334200a2e2e1cc58c
reidssommers-design <reidssommers@gmail.com>
2026-09-17 16:14:41 -0600

    Bondinator Program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("WHat is your first name? ");
+        string fn = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name? ");
+        string ln = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {ln}, {fn} {ln}!");
     }
 }
